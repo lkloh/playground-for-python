5b67214a6dde8bc75e9dc6fe9c10afbc39f848fb
LK Loh <lkloh2410@gmail.com>
2017-09-06 16:55:09 -0700

    saved

--- time_turning.py
+++ time_turning.py
@@ -1,12 +1,14 @@
 from freezegun import freeze_time
 from datetime import datetime
 
-
 def basic():
 	obama_birthday = datetime(year=1961, month=8, day=4)
 	freezer = freeze_time(obama_birthday)
 	freezer.start()
+
 	assert datetime.now() == obama_birthday
+
+	freezer.stop()
 
 def move_forward_in_time():
 	obama_birthday = datetime(year=1961, month=8, day=4)
@@ -15,10 +17,13 @@ def move_forward_in_time():
 		assert datetime.now() == obama_birthday
 		frozen_datetime.move_to(election_day)
 		assert datetime.now() == election_day
-	
 
+def failed_test():
+	obama_birthday = datetime(year=1961, month=8, day=4)
+	print datetime.now()
+	assert datetime.now() == obama_birthday
 
 if __name__ == '__main__':
 	basic()
 	move_forward_in_time()
-
+	failed_test()
