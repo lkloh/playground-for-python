8e50a10fb3f850cc1e1af9b27049ea1f829b208f
LK Loh <lkloh2410@gmail.com>
2017-10-02 15:55:43 -0700

    return eventlet test

--- eventlet_next.py
+++ eventlet_next.py
@@ -12,11 +12,26 @@ def wait_one_second():
 	return "wait_one_second"
 
 
+def test_iter():
+	pile = eventlet.GreenPile(10)
+	pile.spawn(return_immediately)
+	pile.spawn(wait_one_second)
+	return [p for p in pile][0]
+
 def test_next():
+	pile = eventlet.GreenPile(10)
+	pile.spawn(return_immediately)
+	pile.spawn(wait_one_second)
+	return pile.next()
+
+def test_return_not_null():
 	pile = eventlet.GreenPile(10)
 	pile.spawn(return_immediately)
 	pile.spawn(wait_one_second)
 	return [p for p in pile][1]
 
+
 if __name__ == '__main__':
-	print test_next()
+	# print test_iter()
+	# print test_next()
+	print test_return_not_null()
