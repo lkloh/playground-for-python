7ae1209aceb241d810b1927986e2302ad8872569
LK Loh <lkloh2410@gmail.com>
2017-10-03 15:49:36 -0700

    two ways to patch

--- patch_random.py
+++ patch_random.py
@@ -4,9 +4,15 @@ import random
 from unittest.mock import patch
 
 @patch.object(random, 'random')
-def get_random_float(test_random):
-	test_random.return_value = 0.5
+def get_random_float_patch_object(test_random):
+	test_random.return_value = 0.7
+	print(random.random())
+
+@patch('random.random')
+def get_random_float_patch(mock_random):
+	mock_random.return_value = 0.5
 	print(random.random())
 
 if __name__ == '__main__':
-	get_random_float()
+	# get_random_float_patch()
+	get_random_float_patch_object()
