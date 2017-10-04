ec0d6a0002f1bb03487c807ef3a95c06e9b33a9b
LK Loh <lkloh2410@gmail.com>
2017-10-04 12:11:19 -0700

    mock example

--- patch_random.py
+++ patch_random.py
@@ -2,6 +2,7 @@
 # Reference: https://stackoverflow.com/questions/18191275/using-pythons-mock-patch-object-to-change-the-return-value-of-a-method-called-w
 
 import random
+import math
 from unittest.mock import patch
 
 @patch.object(random, 'random')
@@ -14,6 +15,16 @@ def get_random_float_patch(mock_random):
 	mock_random.return_value = 0.5
 	print(random.random())
 
+
+@patch.object(math, 'cos')
+@patch.object(random, 'random')
+def patch_order(mock_random, mock_math):
+	mock_random.return_value = 0.7
+	mock_math.return_value = 4.0 # -1 <= cos(x) <= 1 in practice
+	print(random.random())
+	print(math.cos(0))
+
 if __name__ == '__main__':
 	# get_random_float_patch()
-	get_random_float_patch_object()
+	# get_random_float_patch_object()
+	patch_order()
