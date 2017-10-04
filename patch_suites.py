a698387aa155794a15cafcc55f93e35066653097
LK Loh <lkloh2410@gmail.com>
2017-10-04 16:17:45 -0700

    run test suite

---
+++ patch_suites.py
@@ -0,0 +1,25 @@
+
+import random
+import math
+import unittest
+from unittest.mock import patch
+
+class TestPatchClass(unittest.TestCase):
+
+    def test_one(self):
+        print(random.random())
+
+    def test_two(self):
+        print(random.random())
+
+    def test_three(self):
+        print(random.random())
+
+if __name__ == '__main__':
+    with patch.object(random, 'random') as mock_random:
+        mock_random.return_value = 0.5
+        unittest.main()
+
+    with patch.object(random, 'random') as mock_random:
+        mock_random.return_value = 0.7
+        unittest.main()
